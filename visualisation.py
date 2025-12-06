6e0f7ab37cd0c8d886e78f7fc823329ba26a241b
sofiia-da <demenko.pn@ucu.edu.ua>
2025-12-06 20:30:46 +0200

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -1,17 +1,22 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-edge_color1= {'v1': 'green', 'v2': 'red', 'v3': 'blue'}
-nodes = {'v1' : ['v2'], 'v2': ['v1'], 'v3': []}
+edge_color1= {'1': 'red', '2': 'green', '3': 'blue', '4':'red', '5': 'green'} #result of "" function
+nodes = {'1' : ['2', '5'], '2': ['3', '1'], '3': ['4', '2'], '4':['5', '3'], '5':['1', '4']} #result of "" function
 
-G = nx.from_dict_of_lists(nodes)
+
+G = nx.from_dict_of_lists(nodes) 
 nx.set_node_attributes(G, edge_color1, 'color') 
 
 
 
-def visualize_adjacency_list_graph(G, filename):
+def visualize_adjacency_list_graph(G, filename: str, status: str):
+    color_map = nx.get_node_attributes(G, 'color')
+    node_color_list = [color_map[node] for node in G.nodes()]
+    
+    num_nodes = G.number_of_nodes()
+    node_size = 600 if num_nodes <= 10 else 250
 
-    # Визначення позицій
     pos = nx.spring_layout(G, k=0.8, iterations=50)
 
     plt.figure(figsize=(12, 8))
@@ -19,20 +24,22 @@ def visualize_adjacency_list_graph(G, filename):
         G,
         pos,
         with_labels=True,
-        node_color=['green', 'red', 'blue'],
-        node_size=300,
+        node_color=node_color_list,
+        node_size=node_size,
         font_weight='bold',
         edge_color='dimgray',
         edgecolors='black'
         )
 
-
-    plt.title("Візуалізація Трикольорового Графа", size=18)
-    plt.axis('off') #вимикаємо осі координат
+    if status == 'old':
+        text = 'Visualisation of new correct graph'
+    else:
+        text = 'Visualisation of graph with initial (wrong) colors'
+    plt.title(text, size=18)
+    plt.axis('off')
 
     plt.savefig(filename, format="png", bbox_inches="tight") 
-    print(f"\n Граф збережено у файл: {filename}")
+    print(f"\nPicture of the graph is saved in: {filename}")
 
-    # plt.show() якщо хочете побачити зображення на екрані
 
-visualize_adjacency_list_graph(G)
+visualize_adjacency_list_graph(G, 'graph.png')
