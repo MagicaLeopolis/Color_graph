5e2754ff0370b30c4f75c54a426ce1421c5d9082
JOURA0 <jurii080ds@gmail.com>
2025-12-11 23:08:33 +0200

    Update args_pars.py

--- args_pars.py
+++ args_pars.py
@@ -1,37 +1,67 @@
-import argparse
+'''
+main
+python main.py --file testing3(edges).csv --img my_graph.png
+or :
+python main.py --file testing3(edges).csv --img my_graph.png --with_colors correct_colors.csv
+'''
 
-from create_dict_right_color_final import create_dict_right_color
-from read_file import read_file, write_matrix
-from Our_main_cod_without_color import backtrack_color
+import argparse
 from visualisation import visualize_adjacency_list_graph
+import Our_main_cod_without_color as f
+import backtrack_with_colors as extra
 
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description="Graph 3-coloring tool")
 
-    parser.add_argument("--file", required=True, help="CSV файл графа")
-    parser.add_argument("--img", default="graph.png", help="Куди зберегти зображення")
+    parser.add_argument("--file", required=True, help="CSV файл з ребрами графа")
+    parser.add_argument("--img", default="graph.png", help="Куди зберегти зображення графа")
+    parser.add_argument("--with_colors", help="CSV файл із початковими кольорами (опціонально)")
 
     args = parser.parse_args()
 
-    dic = read_file(args.file)
-    print("Словник суміжності:", dic)
-
-    matrix, keys = write_matrix(dic)
-    print("Матриця:", matrix)
-
-    result = backtrack_color(0, dic, ['red', 'green', 'blue'])
-    print("Backtracking:", result)
-
-    index_colors = {i: result[key] for i, key in enumerate(keys)}
-    fixed = create_dict_right_color(index_colors, matrix)
-
-    if fixed is None:
-        print("Граф неможливо розфарбувати")
-        exit()
-
-    final_colors = {keys[i]: fixed[i] for i in range(len(keys))}
+    dic = f.read_file(args.file)
+    print("\n Словник суміжності:")
     print(dic)
 
-    visualize_adjacency_list_graph(final_colors, dic, args.img)
+    f.correct_color = {}
+
+    print("\n Виконуємо backtracking (стандартний)")
+    result = f.backtrack_color(0, dic, ["red", "green", "blue"])
+
+    if result is None:
+        print("\n Граф неможливо розфарбувати.")
+        exit()
+
+    print("\n Результат розфарбування (основний):")
+    print(result)
+
+    f.write_color_graph(result)
+    print("\n Кольори збережено у corect_colors.txt")
+
+    visualize_adjacency_list_graph(result, dic, args.img)
+    print("\n Граф збережено у:", args.img)
+
+
+    if args.with_colors:
+        print("\n==========================")
+        print(" РЕЖИМ З КОРЕКТНИМИ КОЛЬОРАМИ")
+        print("==========================")
+
+        start_colors = extra.color_start(args.with_colors)
+        print("\n Початкові (неправильні) кольори:")
+        print(start_colors)
+
+        print("\n Виконуємо backtracking_with_colors ...")
+
+        result2 = extra.backtrack_color(dic=dic,d=start_colors,colors=["red", "green", "blue"],\
+                                        correct_colar={},start=0)
+
+        if result2 is None:
+            print("\n Другий backtracking: неможливо знайти розфарбування.")
+        else:
+            print("\n Результат другого алгоритму (виправлені кольори):")
+            print(result2)
+
+        print("\n РЕЖИМ З КОЛЬОРАМИ ЗАВЕРШЕНО.")
